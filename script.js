d2c4a39f872c43edd9ad50d20ff6ee931a3c79e3
alexei <webdesfreelance@gmail.com>
2020-04-23 09:33:28 +0300

    initial save

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let numberData=777;
+typeof 777;
+let stringData='String Data Type';
+let undefinedData
+let booleanData = true;
+let nullData = null;
+
+
+
+
+console.log(typeof numberData);
+console.log(typeof stringData);
+console.log(typeof undefinedData);
+console.log(typeof booleanData);
+console.log(typeof nullData);
+
+
+
+
